8f5d64f7529bab7d49e28f080077b92cedf1428b
kavinmani93 <kavinmani93@gmail.com>
2015-12-30 15:12:31 -0600

    First commit

---
+++ Staircase.cpp
@@ -0,0 +1,41 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++)
+        {
+        for(int k=n-1;k>i;k--)
+                cout<<" ";
+        for(int j=0;j<=i;j++)
+            {
+                cout<<"#";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
