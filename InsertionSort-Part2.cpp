8eabbe432ee265e3d55d3d671ab5ca0d40de588b
kavinmani93 <kavinmani93@gmail.com>
2016-01-09 22:01:36 -0600

    First commit

---
+++ InsertionSort-Part2.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <cassert>
+#include <iostream>
+using namespace std;
+void insertionSort(int n, int *  a) {
+
+    for(int i=1;i<n;i++)
+    {
+        int j=0;
+        while(j<i)
+        {
+            if(a[j]>a[i])
+            {
+                int temp = a[j];
+                a[j] = a[i];
+                a[i] = temp;
+            }
+            j++;
+        }
+        for(int k=0;k<n;k++)
+            cout<<a[k]<<" ";
+        cout<<"\n";
+    }
+
+}
+int main(void) {
+   
+    int _ar_size;
+    cin >> _ar_size;
+    //scanf("%d", &_ar_size);
+    int _ar[_ar_size], _ar_i;
+    for(_ar_i = 0; _ar_i < _ar_size; _ar_i++) { 
+        cin >> _ar[_ar_i];
+        //scanf("%d", &_ar[_ar_i]); 
+    }
+
+   insertionSort(_ar_size, _ar);
+   
+   return 0;
+}
