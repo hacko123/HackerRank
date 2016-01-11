9f0381a451786f7f39796b3ed6698feb6309e6a3
kavinmani93 <kavinmani93@gmail.com>
2016-01-10 22:07:15 -0600

    First commit

---
+++ ServiceLane.cpp
@@ -0,0 +1,50 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    int min=0;
+    int t;
+    cin >> n >> t;
+    vector<int> width(n);
+    for(int width_i = 0;width_i < n;width_i++){
+       cin >> width[width_i];
+    }
+    for(int a0 = 0; a0 < t; a0++){
+        int a;
+        int b;
+        cin >> a >> b;
+        min = width[a];
+        for(int i=a;i<=b;i++)
+        {
+            if(min > width[i])
+                 min = width[i];
+        }
+        cout << min << "\n";
+        min = 0;
+    }
+    return 0;
+}
